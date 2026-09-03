bbe5943121662c767d8477f175cb7ae6fdeadee9
Hatice Sarimustafaoglu <haticesari6107@gmail.com>
2026-09-03 12:12:39 +0300

    feat: table ve user vuex store modulleri eklendi

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,8 @@
 import { createStore } from 'vuex';
 import notification from './notification';
 import database from './database';
+import table from './table';
+import user from './user';
 
 export default createStore({
   modules: {
